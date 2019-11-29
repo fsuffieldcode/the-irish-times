b1ce3d23ed8f7dfd2f87a5d669b57e9b52c88b2d
fsuffieldcode <fsuffieldcode@gmail.com>
2019-11-29 22:48:40 +0000

    styling and add nav scroll js interactivity

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 
 let scrollPos = 0;
 const mainNav = document.querySelector('.main-nav');
+const date = document.querySelector('.date');
+const weather = document.querySelector('.weather');
+const signInButton = document.querySelector('.sign-in-button');
+const subscribeButton = document.querySelector('.subscribe-button');
+const hamburger = document.querySelector('.hamburger-link');
+
 
 function checkPosition() {
     let windowY = window.scrollY;
     if (scrollPos !== windowY) {
         mainNav.classList.add('main-nav-shrink');
+        date.classList.add('hide');
+        weather.classList.add('hide');
+        subscribeButton.classList.add('hide');
+        signInButton.classList.add('hide');
+        hamburger.classList.remove('hide');
+
     }
     scrollPos = windowY;
     if (windowY === 0) {
         mainNav.classList.remove('main-nav-shrink');
+        date.classList.remove('hide');
+        weather.classList.remove('hide');
+        subscribeButton.classList.remove('hide');
+        signInButton.classList.remove('hide');
     }
   }
   
