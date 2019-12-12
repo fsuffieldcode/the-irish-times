5f6f6bdecd861ccf7cbecfd5bfca151e236b702d
fsuffieldcode <fsuffieldcode@gmail.com>
2019-12-12 16:06:01 +0000

    fix date function

--- script.js
+++ script.js
@@ -47,102 +47,98 @@ function checkPosition() {
         dateContainer.classList.remove('hide');
         searchContainer.classList.remove('big-button');
     }
-  }
-  
-  window.addEventListener('scroll', checkPosition);
-  
+}
+
+window.addEventListener('scroll', checkPosition);
 
 
-//   const now = new Date();
 
-// const day = now.getDay();
-// const month = now.getMonth();
-// const date = now.getDate();
-// const year = now.getFullYear();
+const now = new Date();
 
-
-// let dayName = "Sunday";
-// let monthName = "January";
-// let yearName = "1999";
-// let dateString = "";
+const day = now.getDay();
+const month = now.getMonth();
+let dateNumber = now.getDate();
+const year = now.getFullYear();
 
 
-// const getDayName = (day) => {
-//     switch (day) {
-//         case 0:
-//             dayName = "Sunday";
-//             break;
-//         case 1:
-//             dayName = "Monday";
-//             break;
-//         case 2:
-//             dayName = "Tuesday";
-//             break;
-//         case 3:
-//             dayName = "Wednesday";
-//             break;
-//         case 4:
-//             dayName = "Thursday";
-//             break;
-//         case 5:
-//             dayName = "Friday";
-//             break;
-//         case 6:
-//             dayName = "Saturday";
-//             break;
-//     }
-//     dayName = dayName.slice(0,3);
-//     return dayName;
-// }
+let dayName = "Sunday";
+let monthName = "January";
+let yearName = "1999";
+let dateString = "";
 
-// const getMonthName = (month) => {
-//     switch (month) {
-//         case 0:
-//             monthName = "January";
-//             break;
-//         case 1:
-//             monthName = "February";
-//             break;
-//         case 2:
-//             monthName = "March";
-//             break;
-//         case 3:
-//             monthName = "April";
-//             break;
-//         case 4:
-//             monthName = "May";
-//             break;
-//         case 5:
-//             monthName = "June";
-//             break;
-//         case 6:
-//             monthName = "July";
-//             break;
-//         case 7:
-//             monthName = "August";
-//             break;
-//         case 8:
-//             monthName = "September";
-//             break;
-//         case 9:
-//             monthName = "October";
-//             break;
-//         case 10:
-//             monthName = "November";
-//             break;
-//         case 11:
-//             monthName = "December";
-//             break;
-//     }
-//     monthName = monthName.slice(0, 3);
-//     return monthName;
-// }
 
-// console.log(dayName + ", " + MonthName + " " + date + ", " + year)
+const getDayName = (day) => {
+    switch (day) {
+        case 0:
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+    }
+    dayName = dayName.slice(0, 3);
+    return dayName;
+}
 
-// function getDateString() {
-//     getDayName();
-//     getMonthName();
-//     dateString = 
-//     con dateString;
-// }
+const getMonthName = (month) => {
+    switch (month) {
+        case 0:
+            monthName = "January";
+            break;
+        case 1:
+            monthName = "February";
+            break;
+        case 2:
+            monthName = "March";
+            break;
+        case 3:
+            monthName = "April";
+            break;
+        case 4:
+            monthName = "May";
+            break;
+        case 5:
+            monthName = "June";
+            break;
+        case 6:
+            monthName = "July";
+            break;
+        case 7:
+            monthName = "August";
+            break;
+        case 8:
+            monthName = "September";
+            break;
+        case 9:
+            monthName = "October";
+            break;
+        case 10:
+            monthName = "November";
+            break;
+        case 11:
+            monthName = "December";
+            break;
+    }
+    monthName = monthName.slice(0, 3);
+    return monthName;
+}
+
+getDayName(day);
+getMonthName(month);
+
+const formattedDate = dayName + ", " + monthName + " " + dateNumber + ", " + year;
